caef67a3f6d967682f649c6a443dbd44da387ab4
nosanity <no-sanity17@ya.ru>
2018-12-13 18:02:12 +0300

    PLP-1384 Ошибка выполнения команды отправки массовых писем

--- extension_email/management/commands/send_support_mail.py
+++ extension_email/management/commands/send_support_mail.py
@@ -3,7 +3,6 @@
 import logging
 import sys
 import tempfile
-from optparse import make_option
 from django.core.management.base import BaseCommand
 from post_office.lockfile import FileLock, FileLocked
 from extension_email.notifications import send_queued
@@ -13,10 +12,13 @@ default_lockfile = tempfile.gettempdir() + "/support_email"
 
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option('-L', '--lockfile', type='string', default=default_lockfile,
-                    help='Absolute path of lockfile to acquire'),
-    )
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-L', '--lockfile',
+            type=str,
+            default=default_lockfile,
+            help='Absolute path of lockfile to acquire'
+        )
 
     def handle(self, *args, **options):
         try:
@@ -25,6 +27,5 @@ class Command(BaseCommand):
                     send_queued()
                 except Exception as e:
                     logging.error(e, exc_info=sys.exc_info(), extra={'status_code': 500})
-                    raise
         except FileLocked:
             logging.info('Failed to acquire lock, terminating now.')
